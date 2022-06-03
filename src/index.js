cb680a8d7602336cdbdb94957ab82b34e1dde983
Jonathan-AG <Jonathan.martinez@aggroup.com.mx>
2022-06-03 00:00:48 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import { Server as WebSocketServer } from 'socket.io';
 import http from 'http';
 
-let sessions = [];
 const app = express();
 const server = http.createServer(app);
 const io = new WebSocketServer(server);
