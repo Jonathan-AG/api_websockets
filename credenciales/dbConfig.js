cfc8e2c4c706eb7039cb0b768cbe5eb594680681
Jonathan-AG <Jonathan.martinez@aggroup.com.mx>
2022-07-06 14:40:46 -0500

    Add credentials and database config

---
+++ credenciales/dbConfig.js
@@ -0,0 +1,27 @@
+module.exports = {
+    localhost: '127.0.0.1',
+    mysqlConfig: {
+        user: 'usercoppel',
+        password: 'gJaw6N2Cy53OJ88EZls63k40NKYidzv6',
+        database: 'escolar',
+        port: 9876
+    },
+    sshTunnelConfig: {
+        username: 'literaturas',
+        password: 'AJlajskjso3%',
+        host: 'agcollege.edu.mx',
+        port: 22
+    },
+    mysqlConfigDesarrollo: {
+        user: 'root',
+        password: 'UEJdjck5786Ji.',
+        database: 'escolar',
+        port: 9876
+    },
+    sshTunnelConfigDesarrollo: {
+        username: 'root',
+        password: 'GJkuriu45klld.',
+        host: '164.90.144.135',
+        port: 22
+    }
+}
--- src/record_educativo.js
+++ src/record_educativo.js
@@ -2,11 +2,17 @@ module.exports = {
     messages: function (io) {
         io.on('connection', async function (socket) {
             //Variables
+            let activatePages = [];
 
             //Listeners
             socket.on('activePage', async function (activePage) {
                 console.log("Active page is: " + activePage);
             });
+
+            socket.on('disconnect', async function () {
+                console.log("Socket disconnected");
+                console.log("Save data");
+            })
 
             //Emitters
         });
