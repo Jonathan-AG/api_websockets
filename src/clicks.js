219a435097d04fd2fc4cc34cc70d928682a6dc37
Jonathan-AG <Jonathan.martinez@aggroup.com.mx>
2022-10-26 18:38:33 -0500

    Update API

--- src/clicks.js
+++ src/clicks.js
@@ -1,7 +1,19 @@
 module.exports = {
-    clicks: function (io, conn) {
+    countClicks: function (io, conn) {
         io.on("connection", async function(socket) {
-            console.log("Soy clicks");
+            const usuario = socket.handshake.query;
+
+            socket.on("click", async function(values) {
+                let created_at = moment().tz('America/Mazatlan').format("YYYY/MM/DD HH:mm:ss");
+
+                let query = `
+                    CALL escolar.sp_setClicks(${usuario.id_plan_estudio}, ${usuario.id_moodle}, '${values.origen}', '${values.valor_origen}', '${created_at}');
+                `;
+
+                conn.invokeQuery(query, function(results) {
+                    //console.log(results);
+                });
+            });
         });
     }
 };
