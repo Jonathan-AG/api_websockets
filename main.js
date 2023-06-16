f7359867cf6e52526a426af71c8aff4a6ce60f09
Jonathan-AG <Jonathan.martinez@aggroup.com.mx>
2023-06-15 18:54:41 -0600

    Update

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 import express from 'express';
 import http from "http";
-import { Server } from "socket.io";
-import DatabaseSingleton from './tools/databaseSingleton.js';
-import activeSessionsSocket from "./src/sessions.js";
+//import { Server } from "socket.io";
+//import DatabaseSingleton from './tools/databaseSingleton.js';
+//import activeSessionsSocket from "./src/sessions.js";
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
-const dbSingleton = new DatabaseSingleton();
-const session = io.of("session");
+//const io = new Server(server);
+//const dbSingleton = new DatabaseSingleton();
+//const session = io.of("session");
 
-activeSessionsSocket(session, dbSingleton);
+//activeSessionsSocket(session, dbSingleton);
 
 app.get("/hello", function(req, res) {
   res.status(200).send("Hello World!");
